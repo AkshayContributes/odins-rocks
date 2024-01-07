4c860ffb7243502752cd904191273b08290644b7
akshay-thakur-dc <akshay@aksact.dev>
2024-01-07 23:56:09 +0530

    added gui

--- script.js
+++ script.js
@@ -1,78 +1,102 @@
 'use strict';
 
-function getComputerChoice(){
-    const choices = ['rock', 'paper', 'scissors'];
-    const randomNumber = Math.floor(Math.random() * 3);
-    return choices[randomNumber];
+
+let playerChoice;
+let computerChoice;
+
+const playerChoiceDiv = document.getElementById('player-choice');
+const computerChoiceDiv = document.getElementById('computer-choice');
+let playerScore = document.getElementById('player-score-id');
+let computerScore = 0;
+
+function makeComputerChoice() {
+  const choices = [0x270A, 0x270B, 0x270C];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return String.fromCodePoint(choices[randomIndex]);
 }
 
-function getPlayerChoice(){
-    return prompt('Rock, Paper or Scissors?'); 
+function resetGame() {
+  computerScore = 0;
+  playerScore = 0;
+  document.getElementById('computer-curr-score').innerHTML = computerScore;
+  document.getElementById('player-curr-score').innerHTML = playerScore;
+  const resultBox = document.getElementById('result-container');
+  const classListResultBox = resultBox.classList;
+  classListResultBox.add('hidden');
+  document.getElementById('result-text').innerHTML = '';
+}
+
+function showResultBox() {
+  const classResultBox = document.getElementById('result-container').classList;
+  classResultBox.remove('hidden');
+
+}
+
+function computerWins() {
+
+  console.log(computerScore);
+  computerScore++;
+  document.getElementById('computer-curr-score').innerHTML = computerScore;
+  if (computerScore === 5) {
+    showResultBox();
+    document.getElementById('result-text').innerHTML = 'Computer Wins';
+  }
+
+}
+
+function playerWins() {
+
+  console.log(playerScore);
+  playerScore++;
+  document.getElementById('player-curr-score').innerHTML = playerScore;
+  if (playerScore === 5) {
+    showResultBox();
+    document.getElementById('result-text').innerHTML = 'Player Wins';
+  }
+
+}
+
+function calculateWinner(playerChoice, computerChoice) {
+  if (playerChoice === String.fromCodePoint(0x270A) && computerChoice === String.fromCodePoint(0x270B)) {
+    computerWins();
+  } else if (playerChoice === String.fromCodePoint(0x270A) && computerChoice === String.fromCodePoint(0x270C)) {
+    playerWins();
+  } else if (playerChoice === String.fromCodePoint(0x270B) && computerChoice === String.fromCodePoint(0x270A)) {
+    playerWins();
+
+  } else if (playerChoice === String.fromCodePoint(0x270B) && computerChoice === String.fromCodePoint(0x270C)) {
+    computerWins();
+
+  } else if (playerChoice === String.fromCodePoint(0x270C) && computerChoice === String.fromCodePoint(0x270A)) {
+    computerWins();
+
+  } else if (playerChoice === String.fromCodePoint(0x270C) && computerChoice === String.fromCodePoint(0x270B)) {
+    playerWins();
+
+  }
 }
 
 
-function determineWinner(userChoice, computerChoice){
-    console.log(userChoice, computerChoice);
-    if(userChoice === 'rock' && computerChoice === 'paper'){
-        return [-1, 'You Lose! Paper beats Rock'];
+function play(buttonId) {
+  return new Promise((resolve, reject) => {
+    if (buttonId === 'rock') {
+      playerChoice = String.fromCodePoint(0x270A);
+    } else if (buttonId === 'paper') {
+      playerChoice = String.fromCodePoint(0x270B);
+    } else if (buttonId === 'scissors') {
+      playerChoice = String.fromCodePoint(0x270C);
+    } else {
+      reject(new Error('Invalid buttonId'));
     }
 
-    if(userChoice === 'paper' && computerChoice === 'scissors'){
-        return [-1, 'You Lose! Scissors beats Paper'];
-    }
+    playerChoiceDiv.innerHTML = playerChoice;
+    computerChoice = makeComputerChoice();
+    computerChoiceDiv.innerHTML = computerChoice;
 
-    if(userChoice === 'scissors' && computerChoice === 'rock'){
-        return [-1, 'You Lose! Rock beats Scissors'];
-    }
-
-    if(userChoice === 'rock' && computerChoice === 'scissors'){
-        return [1, 'You Win! Rock beats Scissors'];
-    }
-
-    if(userChoice === 'paper' && computerChoice === 'rock'){
-        return [1, 'You Win! Paper beats Rock'];
-    }
-
-    if(userChoice === 'scissors' && computerChoice === 'paper'){
-        return [1, 'You Win! Scissors beats Paper'];
-    }
-
-
-    return [0, 'It\'s a tie!'];
+    calculateWinner(playerChoice, computerChoice);
+  });
 }
 
 
-function playGame(){
-    try{
-        const userInput = getPlayerChoice();
-        if(userInput !== null){
-            const computerChoice = getComputerChoice();
-            const result = determineWinner(userInput, computerChoice);
-            console.log(result);
-            return result;
-            
-        }else{
-            console.log('You cancelled the game');
-        }
-    }catch(error){
-        console.error(`error ocurred ${error} `);
-    }
-}
 
-function game(){
-    let playerWin = 0, computerWin = 0;
-    for(let i = 0; i < 5; i++){
-        const result = playGame()[0];
-        console.log(result);
-        if(result === 1){
-            playerWin++;
-        }else if(result === -1){
-            computerWin++;
-        }
 
-        console.log(`Player wins: ${playerWin} and Computer wins: ${computerWin}`);
-
-    }
-}
-
-game();
